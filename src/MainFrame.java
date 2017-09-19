9aba74892a7f994bc54d663d7162eff02d997f92
q9002285117 <105021038@live.asia.edu.tw>
2017-09-19 12:31:55 +0800

    firsr commit

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,7 +5,10 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
-    private Button eb=new Button("EXIT");
+    private Button btnexit=new Button("EXIT");
+    private Button btnadd=new Button("ADD");
+    private Label lab=new Label("0");
+    private int n=0;
     public MainFrame(){
     init();
 }
@@ -14,18 +17,31 @@ private void init(){
 //    this.setSize(50,100);
     this.setBounds(100,200,350,200);
     this.setLayout(null);
-    eb.setBounds(130,100,80,30);
-    this.add(eb);
+    btnexit.setBounds(130,100,80,30);
+    btnadd.setBounds(130,70,80,30);
+    lab.setBounds(160,130,80,30);
+    btnexit.setBackground(new Color(252, 182, 255));
+    this.add(btnexit);
+    this.add(btnadd);
+    this.add(lab);
     this.addWindowListener(new WindowAdapter() {
         @Override
         public void windowClosing(WindowEvent windowEvent) {
             System.exit(0);
         }
     });
-    eb.addActionListener(new ActionListener() {
+
+    btnexit.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
             System.exit(0);
+        }
+    });
+    btnadd.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            n++;
+            lab.setText(Integer.toString(n));
         }
     });
 }
