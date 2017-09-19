f570d3c898733c71d716352ef40486480ae37741
q9002285117 <105021038@live.asia.edu.tw>
2017-09-19 11:57:38 +0800

    firsr commit

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,17 +1,30 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
-public MainFrame(){
+    private Button eb=new Button("EXIT");
+    public MainFrame(){
     init();
 }
 private void init(){
-    this.setLocation(100,200);
-    this.setSize(50,100);
+//    this.setLocation(100,200);
+//    this.setSize(50,100);
+    this.setBounds(100,200,350,200);
+    this.setLayout(null);
+    eb.setBounds(130,100,80,30);
+    this.add(eb);
     this.addWindowListener(new WindowAdapter() {
         @Override
         public void windowClosing(WindowEvent windowEvent) {
+            System.exit(0);
+        }
+    });
+    eb.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
             System.exit(0);
         }
     });
